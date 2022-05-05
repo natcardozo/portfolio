5b41c6ccbc35389d77aed17505347c38da865874
Nathalia Cardozo <nathaliacardozosilva@gmail.com>
2022-05-05 11:01:32 -0300

    responsividade

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,10 @@
 import './header.css';
 import logo from '../../assets/imagens/logo.png';
 import { Link } from 'react-router-dom';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import HomeIcon from '@mui/icons-material/Home';
+import ArticleIcon from '@mui/icons-material/Article';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 export default function Header() {
     return (
@@ -12,6 +16,25 @@ export default function Header() {
                     <Link className='header__link' to="/repositorios">Repositórios</Link>
                     <Link className='header__link' to="/contato">Contato</Link>
                 </nav>
+                <div className='header__navigation__botao--mobile'>
+                    <KeyboardArrowDownIcon sx={{ color: "#ffffff", mt: "1rem", fontSize: "2rem" }} />
+                </div>
+                <div className='header__navigation--mobile'>
+                    <nav className='header__navigation--mobile' id='navigation--mobile'>
+                        <div className='header__navigation__item--mobile'>
+                            <HomeIcon sx={{ color: "#ffffff", width: "25px"}} />
+                            <Link className='header__link--mobile' to="/">Início</Link>
+                        </div>
+                        <div className='header__navigation__item--mobile'>
+                            <ArticleIcon sx={{ color: "#ffffff", width: "25px"}} />
+                            <Link className='header__link--mobile' to="/repositorios">Repositórios</Link>
+                        </div>
+                        <div className='header__navigation__item--mobile'>
+                            <AccountCircleIcon sx={{ color: "#ffffff", width: "25px"}} />
+                            <Link className='header__link--mobile' to="/contato">Contato</Link>
+                        </div>
+                    </nav>
+                </div>
             </div>
         </header>
     )
