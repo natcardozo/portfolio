96936880b3b0df2d07fb3cb2bd125b61c1c394ed
Nathalia Cardozo <nathaliacardozosilva@gmail.com>
2022-07-20 22:35:22 -0300

    organo preview

--- src/pages/Repositorios/index.tsx
+++ src/pages/Repositorios/index.tsx
@@ -1,11 +1,14 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import "./repositorios.css"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./repositorios.css";
+import Aos from "aos";
+import "aos/dist/aos.css"
 
 export default function Repositorios() {
   const [repositorios, setRepositorios] = useState<any[]>([])
 
   useEffect(() => {
+    Aos.init({});
     axios.get('https://api.github.com/users/natcardozo/repos')
       .then(resposta => {
         setRepositorios(resposta.data)
@@ -15,7 +18,7 @@ export default function Repositorios() {
   return (
     <div className='repositorios'>
       {repositorios?.map(repo => (
-        <div key={repo.id} className='repositorio'>
+        <div data-aos="flip-right" key={repo.id} className='repositorio'>
           <a className='repositorio__link' href={repo.html_url}>
             <img className='repositorio__imagem' src={`/assets/imagens_repositorios/${repo.id}.png`} alt={repo.name} />
             <span className='repositorio__nome'>{repo.name.toUpperCase()}</span>
